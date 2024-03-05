2ac33b6e8e4082f9652cfaf7b170cc1e5d232e72
Mathias <email@example.com>
2024-03-05 10:51:44 +0100

    save table content

--- examples/tokio_finfiz_method_third.rs
+++ examples/tokio_finfiz_method_third.rs
@@ -31,14 +31,14 @@ const WEB_XPATH:&[&[&str]] = &[
     //  &["14","eps_qtr_xpath","/html/body/div[4]/table/tbody/tr[3]/td/div/form/table/tbody/tr[4]/td[8]/select/option[3]"],
     //  &["15","peg_xpath","/html/body/div[4]/table/tbody/tr[3]/td/div/form/table/tbody/tr[2]/td[8]/select/option[7]"],
     //  &["16","beta_xpath","/html/body/div[4]/table/tbody/tr[3]/td/div/form/table/tbody/tr[12]/td[6]/select/option[7]"],
-    &["r1","colum_name","/html/body/div[4]/table/tbody/tr[4]/td/div/table/tbody/tr[5]/td/table/tbody/tr/td/table/thead"],
+    // &["r1","colum_name","/html/body/div[4]/table/tbody/tr[4]/td/div/table/tbody/tr[5]/td/table/tbody/tr/td/table/thead"],
     // &["r2","screener_result","/html/body/div[4]/table/tbody/tr[4]/td/div/table/tbody/tr[5]/td/table/tbody/tr/td/table/tbody/tr"],
-     
-         
-];
+    ];
 
 
 fn main() -> color_eyre::Result<(),Box<dyn Error>>  {
+    color_eyre::install()?;
+
     let rt: tokio::runtime::Runtime = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?;
@@ -59,7 +59,8 @@ fn main() -> color_eyre::Result<(),Box<dyn Error>>  {
 
     scrape_all(_driver.clone()).await?;
     screenshot_browser(_driver.clone()).await?;
-    close_browser(_driver.clone()).await?;
+    save_result_table(_driver.clone()).await?;
+    // close_browser(_driver.clone()).await?;
 
     Ok(())
 }
@@ -85,7 +86,7 @@ async fn screenshot_browser(_driver: WebDriver) -> color_eyre::Result<(),Box<dyn
     let mut file = File::create("foo.png")?;
     file.write_all(&screenshot)?;
 
-    println!("Screenshot of browser windows => {:?} ",screenshot);
+    // println!("Screenshot of browser windows => {:?} ",screenshot);
     Ok(())
 }
 
@@ -102,22 +103,6 @@ Ok(())
 
 async fn scrape_all(_driver: WebDriver) ->  color_eyre::Result<(),Box<dyn Error>> {
 
-    //We value your privacy 
-    // let _elem_form: WebElement = _driver
-    //     .find(By::XPath(
-    //         "/html/body/div[1]/div/div/div/div[2]/div/button[3]",
-    //     ))
-    //     .await?;
-    // _elem_form.click().await?;
-
-    // let elem_form: WebElement = _driver
-    //     .find(By::XPath(
-    //         WEB_XPATH[0][2],
-    //        // "/html/body/div[1]/div/div/div/div[2]/div/button[3]",
-    //     ))
-    //     .await?;
-    // elem_form.click().await?;
-
     for field in 0 .. WEB_XPATH.len() {
 
         println!("No.   => {}",WEB_XPATH[field][0]);
@@ -126,17 +111,59 @@ async fn scrape_all(_driver: WebDriver) ->  color_eyre::Result<(),Box<dyn Error>
         let elem_form: WebElement = _driver
         .find(By::XPath(
             WEB_XPATH[field][2],
-           // "/html/body/div[1]/div/div/div/div[2]/div/button[3]",
-        ))
+                   ))
         .await?;
     elem_form.click().await?;
     wait_seconds_of_browser(_driver.clone(),5).await?;
     }
 
-    wait_seconds_of_browser(_driver.clone(),30).await?;
+    wait_seconds_of_browser(_driver.clone(),20).await?;
 
     Ok(())
 }
+
+
+//save_result_table
+async fn save_result_table(_driver: WebDriver) ->  color_eyre::Result<(),Box<dyn Error>> {
+
+    const SAVE_TABLE:&[&[&str]] = &[
+     //No.,FieldName,xpath        
+     &["t1","colum_name","/html/body/div[4]/table/tbody/tr[4]/td/div/table/tbody/tr[5]/td/table/tbody/tr/td/table/thead"],
+     &["t2","No.:","/html/body/div[4]/table/tbody/tr[4]/td/div/table/tbody/tr[5]/td/table/tbody/tr/td/table/tbody/tr"],
+     ];
+
+     
+        //debug
+        let field=0;
+        println!("No.   => {}",SAVE_TABLE[field][0]);
+        println!("Field => {}",SAVE_TABLE[field][1]);
+        println!("XPath => {}",SAVE_TABLE[field][2]);
+
+        let thead_rows_vec:Vec<WebElement> = _driver.find_all(By::XPath(SAVE_TABLE[field][2])).await?;
+        for thead_row in thead_rows_vec{
+            println!("row => {}",thead_row);
+            println!("row Name: {}",thead_row.tag_name().await?);
+            println!("row Text: {}",thead_row.text().await?);
+
+        //debug
+        let field=1;
+        println!("No.   => {}",SAVE_TABLE[field][0]);
+        println!("Field => {}",SAVE_TABLE[field][1]);
+        println!("XPath => {}",SAVE_TABLE[field][2]);
+        
+        let thead_cell_vec:Vec<WebElement> = thead_row.find_all(By::XPath(SAVE_TABLE[field][2])).await?;
+        
+        for thead_cell in thead_cell_vec {
+            println!("cell => {}",thead_cell);
+            println!("cell Name: {}",thead_cell.tag_name().await?);
+            println!("cell Text: {}",thead_cell.text().await?);
+        }
+        }
+
+    Ok(())
+}
+
+// /html/body/div[4]/table/tbody/tr[4]/td/div/table/tbody/tr[5]/td/table/tbody/tr/td/table/tbody/tr[1]/td[1]/a
 
 async fn initialize_driver() -> Result<WebDriver, WebDriverError> {
     let caps = DesiredCapabilities::chrome();
